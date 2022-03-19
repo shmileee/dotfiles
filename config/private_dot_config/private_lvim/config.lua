f373e8c1d567047d8d91538209a65acb32de863d
shmileee <ponomarov.aleksandr@gmail.com>
2022-03-19 14:33:15 +0100

    feature(chezmoi): add lunarvim

---
+++ config/private_dot_config/private_lvim/config.lua
@@ -0,0 +1,184 @@
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "onedarker"
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+
+-- better putting
+lvim.keys.visual_mode["p"] = "pgvy"
+
+lvim.keys.normal_mode["k"] = "gk"
+lvim.keys.normal_mode["j"] = "gj"
+lvim.keys.normal_mode["$"] = "g$"
+lvim.keys.normal_mode["0"] = "g0"
+
+-- better yanking
+lvim.keys.normal_mode["Y"] = "y$"
+lvim.keys.visual_mode["y"] = "ygv<esc>"
+
+-- add your own keymapping
+lvim.keys.normal_mode["<C-p>"] = "<cmd>lua require('telescope.builtin').find_files({cwd = require('telescope.utils').buffer_dir()})<CR>"
+lvim.keys.normal_mode["<Space><Space>"] = "<cmd>nohlsearch<CR>"
+lvim.keys.normal_mode["o"] = "o<Esc>"
+lvim.keys.normal_mode["O"] = "O<Esc>"
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
+
+-- Use which-key to add extra bindings with the leader-key prefix
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["n"] = { "<cmd>normal! mz[s1z=`z<CR>", "Fix spelling" }
+lvim.builtin.which_key.mappings["f"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" }
+lvim.builtin.which_key.mappings["r"] = { ":%s///g<Left><Left>", "Replace" }
+lvim.builtin.which_key.mappings["e"] = {}
+lvim.builtin.which_key.mappings["q"] = {}
+lvim.builtin.which_key.mappings["h"] = {}
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+}
+lvim.builtin.which_key.mappings["st"] = {
+  "<cmd>lua require('telescope.builtin').live_grep({cwd = require('telescope.utils').buffer_dir()})<CR>", "Search text in current directory"
+}
+
+lvim.builtin.dashboard.active = true
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "hcl",
+  "c",
+  "javascript",
+  "json",
+  "make",
+  "markdown",
+  "lua",
+  "python",
+  "typescript",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+-- generic LSP settings
+lvim.lsp.automatic_servers_installation = false
+
+require("lvim.lsp.manager").setup("dockerls", {
+    settings = {
+        docker = {
+            languageserver = {
+                formatter = {
+                    ignoreMultilineInstructions = true,
+                },
+            },
+        },
+    },
+})
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { exe = "black", filetypes = { "python" } },
+  { exe = "isort", filetypes = { "python" } },
+  { exe = "terraform_fmt", filetypes = { "terraform" } },
+  {
+    exe = "prettier",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+    args = { "--print-with", "100" },
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "typescript", "typescriptreact" },
+  },
+}
+
+-- -- set additional linters
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { exe = "flake8", filetypes = { "python" } },
+  {
+    exe = "shellcheck",
+    ---@usage arguments to pass to the formatter
+    filetypes = { "sh" },
+  },
+  {
+    exe = "codespell",
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "javascript", "python" },
+  },
+}
+
+-- Additional Plugins
+lvim.plugins = {
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {
+    "tpope/vim-surround",
+    "tpope/vim-repeat",
+    "farmergreg/vim-lastplace",
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    opt = true,
+    ft = "markdown",
+    run = 'cd app && yarn install'
+  },
+  {
+    "ntpeters/vim-better-whitespace",
+    config = function ()
+      vim.g.strip_whitespace_confirm = "0"
+      vim.g.strip_whitelines_at_eof = "1"
+      vim.g.strip_whitespace_on_save = "1"
+    end
+  },
+}
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+lvim.autocommands.custom_groups = {
+  { "FileType", "dashboard", "DisableWhitespace" },
+  { "BufRead,BufNewFile", "*.md", "setlocal textwidth=80" },
+}
+
+local init_custom_options = function()
+  local custom_options = {
+    relativenumber = true, -- Set relative numbered lines
+    colorcolumn = "80", -- Indent line at what column? Set something like '99999' to not display it
+    shell = "/bin/sh"
+  }
+
+  for k, v in pairs(custom_options) do
+    vim.opt[k] = v
+  end
+end
+init_custom_options()
