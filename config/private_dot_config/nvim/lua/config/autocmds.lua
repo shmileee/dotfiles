ef535cb99658fadaf41ab22ecf455edf91c12ec7
shmileee <ponomarov.aleksandr@gmail.com>
2026-08-31 14:54:22 +0200

    fix(config): remove obsolete workstation settings

--- config/private_dot_config/nvim/lua/config/autocmds.lua
+++ config/private_dot_config/nvim/lua/config/autocmds.lua
@@ -7,14 +7,3 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
-
--- Fix Helm syntax highlighting
-vim.api.nvim_create_autocmd({ "BufRead", "BufEnter" }, {
-  pattern = "*.yaml",
-  callback = function()
-    if vim.bo[vim.api.nvim_get_current_buf()].filetype == "helm" then
-      vim.cmd("TSToggle highlight")
-      vim.cmd("TSToggle highlight")
-    end
-  end,
-})
